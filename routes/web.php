6a41da1be6a7cd22a9e0c092d8c6b04656249000
IhsanLana864 <ihsanlana864@gmail.com>
2024-10-18 08:29:51 +0700

    Website Pengembangan Pusat Studi Bencana

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+    // // contoh route example
+    // Route::get('/akunsaya', function () {
+    //     return view('example');
+    // })->name('example');
