5cc2825e4a5d868652102aab2c7f7d23cc606f7e
IhsanLana864 <ihsanlana864@gmail.com>
2024-10-25 00:38:34 +0700

    menambahkan sebagian routes web.php

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,58 @@ Route::get('/', function () {
     // Route::get('/akunsaya', function () {
     //     return view('example');
     // })->name('example');
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/about-element-1', function () {
+    return view('about-element-1');
+});
+Route::get('/about-element-2', function () {
+    return view('about-element-2');
+});
+Route::get('/about-element-3', function () {
+    return view('about-element-3');
+});
+Route::get('/about-element-4', function () {
+    return view('about-element-4');
+});
+Route::get('/appointment-element', function () {
+    return view('appointment-element');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/books', function () {
+    return view('books');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
+Route::get('/ini', function () {
+    return view('ini');
+});
