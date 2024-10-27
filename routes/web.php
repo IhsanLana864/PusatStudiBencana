7c49f90466073e00a7bf44a82e8e4cde4885f9eb
IhsanLana864 <ihsanlana864@gmail.com>
2024-10-27 19:37:20 +0700

    membuat routes sampai service

--- routes/web.php
+++ routes/web.php
@@ -38,26 +38,105 @@ Route::get('/books', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
-Route::get('/ini', function () {
+Route::get('/counter-element-1', function () {
+    return view('counter-element-1');
+});
+Route::get('/counter-element-2', function () {
+    return view('counter-element-2');
+});
+Route::get('/counter-element-3', function () {
+    return view('counter-element-3');
+});
+Route::get('/counter-element-4', function () {
+    return view('counter-element-4');
+});
+Route::get('/appointment-element', function () {
+    return view('appointment-element');
+});
+Route::get('/error ', function () {
+    return view('error');
+});
+Route::get('/faq-element', function () {
+    return view('faq-element');
+});
+Route::get('/index-2', function () {
+    return view('index-2');
+});
+Route::get('/index-3', function () {
+    return view('index-3');
+});
+Route::get('/index-4', function () {
+    return view('index-4');
+});
+Route::get('/index-5', function () {
+    return view('index-5');
+});
+Route::get('/index-onepage', function () {
+    return view('index-onepage');
+});
+Route::get('/index-rtl', function () {
+    return view('index-rtl');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/intro-element-1.', function () {
+    return view('intro-element-1');
+});
+Route::get('/intro-element-2.', function () {
+    return view('intro-element-2');
+});
+Route::get('/news-element-1', function () {
+    return view('news-element-1');
+});
+Route::get('/news-element-2', function () {
+    return view('news-element-2');
+});
+Route::get('/news-element-3', function () {
+    return view('news-element-3');
+});
+Route::get('/news-element-4', function () {
+    return view('news-element-4');
+});
+Route::get('/news-element-5', function () {
+    return view('news-element-5');
+});
+
+Route::get('/projects-elemet-1', function () {
+    return view('project-element-1');
+});
+Route::get('/projects-elemet-2', function () {
+    return view('project-element-2');
+});
+Route::get('/projects-elemet-3', function () {
+    return view('project-element-3');
+});
+Route::get('/research-details', function () {
+    return view('research-details');
+});
+Route::get('/research', function () {
     return view('ini');
 });
-Route::get('/ini', function () {
+Route::get('/service-details', function () {
     return view('ini');
 });
-Route::get('/ini', function () {
-    return view('ini');
+Route::get('/service-element-1', function () {
+    return view('service-element-1');
 });
-Route::get('/ini', function () {
-    return view('ini');
+Route::get('/service-element-2', function () {
+    return view('service-element-2');
 });
-Route::get('/ini', function () {
-    return view('ini');
+Route::get('/service-element-3', function () {
+    return view('service-element-3');
 });
-Route::get('/ini', function () {
-    return view('ini');
+Route::get('/service-element-4', function () {
+    return view('service-element-4');
 });
-Route::get('/ini', function () {
-    return view('ini');
+Route::get('/service', function () {
+    return view('service');
+});
+Route::get('/skill-element-1', function () {
+    return view('skill-element-1');
 });
 Route::get('/ini', function () {
     return view('ini');
