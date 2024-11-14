e95be59278c2454e7750f5d8b96910f78dde9767
IhsanLana864 <ihsanlana864@gmail.com>
2024-11-14 16:26:36 +0700

    menambahkan maps

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
-<title>Pusat Studi Bencana IPB UNIVERSITY </title>
+    <title>Pusat Studi Bencana IPB UNIVERSITY </title>
 
-<!-- Fav Icon -->
-<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+    <!-- Fav Icon -->
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
 
-<!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,300i,400,400i,600,600i,700,700i,900&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Titillium+Web:300,300i,400,400i,600,600i,700,700i,900&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap"
+        rel="stylesheet">
 
-<!-- Stylesheets -->
-<link href="assets/css/font-awesome-all.css" rel="stylesheet">
-<link href="assets/css/flaticon.css" rel="stylesheet">
-<link href="assets/css/owl.css" rel="stylesheet">
-<link href="assets/css/bootstrap.css" rel="stylesheet">
-<link href="assets/css/jquery.fancybox.min.css" rel="stylesheet">
-<link href="assets/css/animate.css" rel="stylesheet">
-<link href="assets/css/color/theme-color.css" id="jssDefault" rel="stylesheet">
-<link href="assets/css/switcher-style.css" rel="stylesheet">
-<link href="assets/css/style.css" rel="stylesheet">
-<link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- Stylesheets -->
+    <link href="assets/css/font-awesome-all.css" rel="stylesheet">
+    <link href="assets/css/flaticon.css" rel="stylesheet">
+    <link href="assets/css/owl.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/color/theme-color.css" id="jssDefault" rel="stylesheet">
+    <link href="assets/css/switcher-style.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
 
 </head>
 
@@ -32,126 +37,124 @@
 @include('layouts.navbar')
 
 
-    <!--Page Title-->
-    <section class="page-title" style="background-image: url(assets/images/background/page-title-2.jpg);">
-        <div class="auto-container">
-            <div class="row clearfix">
-                <div class="col-lg-8 col-md-12 col-sm-12 content-column">
-                    <div class="content-box clearfix">
-                        <div class="title pull-left">
-                            <h1>Hubungi Kami</h1>
-                        </div>
-                        <ul class="bread-crumb pull-right clearfix">
-                            <li><a href="/index">Home</a></li>
-                            <li><a href="/contact">Contact</a></li>
-                        </ul>
+<!--Page Title-->
+<section class="page-title" style="background-image: url(assets/images/background/page-title-2.jpg);">
+    <div class="auto-container">
+        <div class="row clearfix">
+            <div class="col-lg-8 col-md-12 col-sm-12 content-column">
+                <div class="content-box clearfix">
+                    <div class="title pull-left">
+                        <h1>Hubungi Kami</h1>
                     </div>
+                    <ul class="bread-crumb pull-right clearfix">
+                        <li><a href="/index">Home</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </section>
-    <!--End Page Title-->
+    </div>
+</section>
+<!--End Page Title-->
 
 
-    <!-- contact-section -->
-    <section class="contact-section">
-        <div class="auto-container">
-            <div class="row clearfix">
-                <div class="col-lg-8 col-md-12 col-sm-12 form-column">
-                    <div class="form-inner">
-                        <div class="sec-title left">
-                            <p>Hubungi Kami</p>
-                            <h2>Kami Senang Mendengar Dari Anda.</h2>
-                            <span class="separator"></span>
-                        </div>
-                        <form method="post" action="sendemail.php" id="contact-form" class="contact-form">
-                            <div class="row clearfix">
-                                <div class="col-lg-6 col-md-12 col-sm-12 form-group">
-                                    <input type="text" name="username" placeholder="Your Name" required="">
-                                </div>
-                                <div class="col-lg-6 col-md-12 col-sm-12 form-group">
-                                    <input type="email" name="email" placeholder="Email" required="">
-                                </div>
-                                <div class="col-lg-6 col-md-12 col-sm-12 form-group">
-                                    <input type="text" name="subject" placeholder="Subject" required="">
-                                </div>
-                                <div class="col-lg-6 col-md-12 col-sm-12 form-group">
-                                    <input type="text" name="phone" placeholder="Phone" required>
-                                </div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 form-group">
-                                    <textarea name="message" placeholder="Message"></textarea>
-                                </div>
-                                <div class="col-lg-12 col-md-12 col-sm-12 form-group message-btn">
-                                    <button type="submit" class="theme-btn style-one" name="submit-form">Kirim Sekarang</button>
-                                </div>
+<!-- contact-section -->
+<section class="contact-section">
+    <div class="auto-container">
+        <div class="row clearfix">
+            <div class="col-lg-8 col-md-12 col-sm-12 form-column">
+                <div class="form-inner">
+                    <div class="sec-title left">
+                        <p>Hubungi Kami</p>
+                        <h2>Kami Senang Mendengar Dari Anda.</h2>
+                        <span class="separator"></span>
+                    </div>
+                    <form method="post" action="sendemail.php" id="contact-form" class="contact-form">
+                        <div class="row clearfix">
+                            <div class="col-lg-6 col-md-12 col-sm-12 form-group">
+                                <input type="text" name="username" placeholder="Your Name" required="">
                             </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-12 col-sm-12 info-column">
-                    <div class="info-inner">
-                        <div class="sec-title left">
-                            <p>Informasi Kontak</p>
-                            <h2>Kontak Kami</h2>
-                            <span class="separator"></span>
+                            <div class="col-lg-6 col-md-12 col-sm-12 form-group">
+                                <input type="email" name="email" placeholder="Email" required="">
+                            </div>
+                            <div class="col-lg-6 col-md-12 col-sm-12 form-group">
+                                <input type="text" name="subject" placeholder="Subject" required="">
+                            </div>
+                            <div class="col-lg-6 col-md-12 col-sm-12 form-group">
+                                <input type="text" name="phone" placeholder="Phone" required>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12 form-group">
+                                <textarea name="message" placeholder="Message"></textarea>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12 form-group message-btn">
+                                <button type="submit" class="theme-btn style-one" name="submit-form">Kirim
+                                    Sekarang</button>
+                            </div>
                         </div>
-                        <ul class="info-box clearfix">
-                            <li>
-                                <div class="icon-box"><i class="flaticon-call"></i></div>
-                                <h5>Nomor Telepon</h5>
-                                <p><a href="tel:02518319169">0251-8319169</a></p>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12 col-sm-12 info-column">
+                <div class="info-inner">
+                    <div class="sec-title left">
+                        <p>Informasi Kontak</p>
+                        <h2>Kontak Kami</h2>
+                        <span class="separator"></span>
+                    </div>
+                    <ul class="info-box clearfix">
+                        <li>
+                            <div class="icon-box"><i class="flaticon-call"></i></div>
+                            <h5>Nomor Telepon</h5>
+                            <p><a href="tel:02518319169">0251-8319169</a></p>
+                        </li>
+                        <li>
+                            <div class="icon-box"><i class="flaticon-circular-clock"></i></div>
+                            <h5>Jam Operasional</h5>
+                            <p>8:00 am - 6:00 pm</p>
+                        </li>
+                        <li>
+                            <div class="icon-box"><i class="flaticon-worldwide"></i></div>
+                            <h5>Alamat</h5>
+                            <p>Kantor Pusat Studi Bencana IPB University, Kampus Baranangsiang , Jl. Raya Padjajaran,
+                                Bogor 16144 </p>
+                        </li>
+                    </ul>
+                    <div class="follow-box">
+                        <h5>Ikuti Kami:</h5>
+                        <ul class="social-links clearfix">
+                            <li><a href="https://www.facebook.com/197139333803945/" target="_blank"><i
+                                        class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="mailto:psb@apps.ipb.ac.id"><i class="fab fa-google-plus-g"></i></a></li>
+                            <li><a href="https://www.instagram.com/psb_ipb"><span class="fab fa-instagram"></span></a>
                             </li>
-                            <li>
-                                <div class="icon-box"><i class="flaticon-circular-clock"></i></div>
-                                <h5>Jam Operasional</h5>
-                                <p>8:00 am - 6:00 pm</p>
-                            </li>
-                            <li>
-                                <div class="icon-box"><i class="flaticon-worldwide"></i></div>
-                                <h5>Alamat</h5>
-                                <p>Kantor Pusat Studi Bencana IPB University, Kampus Baranangsiang , Jl. Raya Padjajaran, Bogor 16144 </p>
-                            </li>
+                            <li><a href="https://x.com/IpbPsb"><i class="fab fa-twitter"></i></a></li>
                         </ul>
-                        <div class="follow-box">
-                            <h5>Ikuti Kami:</h5>
-                            <ul class="social-links clearfix">
-                                <li><a href="https://www.facebook.com/197139333803945/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="mailto:psb@apps.ipb.ac.id"><i class="fab fa-google-plus-g"></i></a></li>
-                                <li><a href="https://www.instagram.com/psb_ipb"><span class="fab fa-instagram"></span></a></li>
-                                <li><a href="https://x.com/IpbPsb"><i class="fab fa-twitter"></i></a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- contact-section end -->
+    </div>
+</section>
+<!-- contact-section end -->
 
 
-    <!-- google-map-section -->
-    <section class="google-map-section">
-        <div class="auto-container">
-            <div class="map-column">
-                <div class="google-map-area">
-                    <div
-                        class="google-map"
-                        id="contact-google-map"
-                        data-map-lat="40.712776"
-                        data-map-lng="-74.005974"
-                        data-icon-path="assets/images/icons/map-marker.png"
-                        data-map-title="Brooklyn, New York, United Kingdom"
-                        data-map-zoom="12"
-                        data-markers='{
-                            "marker-1": [40.712776, -74.005974, "<h4>Branch Office</h4><p>77/99 New York</p>","assets/images/shape/map-marker.png"]
-                        }'>
-
-                    </div>
+<!-- google-map-section -->
+<section class="google-map-section">
+    <div class="auto-container">
+        <div class="map-column">
+            <div class="google-map-area">
+                <div style="overflow:hidden;resize:none;max-width:100%;width:1200px;height:600px;">
+                    <div id="display-google-map" style="height:100%; width:100%;max-width:100%;"><iframe
+                            style="height:100%;width:100%;border:0;" frameborder="0"
+                            src="https://www.google.com/maps/embed/v1/directions?origin=Sekolah+Vokasi+IPB&destination=Kantor+Pusat+Studi+Bencana+IPB+University,+Kampus+Baranangsiang+,+Jl.+Raya+Padjajaran,+Bogor+16144+&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+                    </div><a class="googlemaps-made" href="https://www.bootstrapskins.com/themes"
+                        id="authmaps-data">premium bootstrap themes</a>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- google-map-section end -->
+    </div>
+</section>
+<!-- google-map-section end -->
 
 
 x
@@ -191,4 +194,5 @@ x
 <script src="assets/js/script.js"></script>
 
 </body><!-- End of .page_wrapper -->
+
 </html>
